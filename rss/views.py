7e76d66bc6cd82c99bfadb7cd63642ba3ce3f780
Cibrán Docampo <cibdoc@arantia.com>
2018-07-02 14:50:51 +0200

    2018 - Beta Version

--- rss/views.py
+++ rss/views.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 import requests
-from django.utils import encoding
 from django.http import HttpResponseRedirect
 
-MAX_PAGES = 4
-SEARCH_WEB_PAGE = 'https://www.elitetorrent.biz/serie/page/'
+MAX_PAGES = 2
+SEARCH_WEB_PAGE = 'https://www.elitetorrent.biz/series-3/page/'
 DOWNLOAD_URL = 'https://www.elitetorrent.biz/wp-content/uploads/files/'
 PARSER_STRING = '<a href="https://www.elitetorrent.biz/series/'
+PARSER_DOWNLOAD_STRING = '<a href="/wp-content/uploads/files/'
 
 RSS_PATH = 'rss/static/elitetorrent.rss'
 
-def Index(request):
 
+def Index(request):
     if os.path.exists(RSS_PATH):
         os.remove(RSS_PATH)
 
@@ -29,11 +29,16 @@ def Index(request):
         if request.status_code == 200:
             for line in request.content.split('\n'):
                 if PARSER_STRING in line:
+                    individual_request = requests.get(line.split('"')[1])
+                    if request.status_code == 200:
+                        for individual_line in individual_request.content.split('\n'):
+                            if PARSER_DOWNLOAD_STRING in individual_line:
+                                url = 'https://www.elitetorrent.biz' + individual_line.split('"')[1]
                     title = line[len(PARSER_STRING):].split('/')[0]
                     url = DOWNLOAD_URL + title + '.torrent'
                     f.write('<item>\n')
                     f.write(' <title>' + title + '</title>\n')
-                    f.write(' <link>' + url +'</link>\n')
+                    f.write(' <link>' + url + '</link>\n')
                     f.write('</item>\n')
 
     f.write('</channel>\n')
